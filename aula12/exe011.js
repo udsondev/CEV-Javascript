1f11476fcfdc915494883b5bcb968a9845a6af17
Udson <udson.dev@gmail.com>
2019-08-07 23:05:52 -0300

    aula 12

--- aula12/exe011.js
+++ aula12/exe011.js
@@ -1,7 +1,7 @@
-var idade = 16
+var idade = 15
 if (idade < 16) {
     console.log(`Não vota`)
-} else if (idade < 18) {
+} else if (idade < 18 || idade > 65) {
         console.log('Voto opcional')
 } else {
         console.log('Voto obrigatório')
