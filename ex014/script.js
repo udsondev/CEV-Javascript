bf6623704e6238817f65fcb04423f8b446c35c13
Udson <udson.dev@gmail.com>
2019-08-08 04:32:27 -0300

    exercicio aula 12 mudança de hora automatizada

---
+++ ex014/script.js
@@ -0,0 +1,22 @@
+function carregar() {
+    
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manhalogo.png'
+    
+        document.body.style.background = '#ffce96'
+    }
+    else if (hora >=12 && hora <18) {
+        img.src = 'tardelogo.png'
+        document.body.style.background = '#ff755a'
+    }
+    else {
+        img.src = 'noitelogo.png'
+        document.body.style.background = '#06336b'
+    }
+}
