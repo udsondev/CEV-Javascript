5eead1b596a84f039b5e0106c329cdd6e6926eec
Udson <udson.dev@gmail.com>
2019-08-28 02:21:03 -0300

    exercicio 015

--- ex014/script.js
+++ ex014/script.js
@@ -3,7 +3,8 @@ function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 19
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if (hora >= 0 && hora < 12) {
