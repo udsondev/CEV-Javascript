2573fd4875609b9ad0fe72b569bb6fcb133f8b49
Udson <udson.dev@gmail.com>
2019-08-31 05:41:32 -0300

    exe016 feito

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,15 +1,37 @@
 function contar() {
-    var inicio = document.getElementById('inicio').value
-    var fim = document.getElementById('fim').value
-    var passo = document.getElementById('passo').value
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
     var res = document.getElementById('res')
 
-    if (inicio.value.length == 00 || fim.value.length ==0 || passo.value.length == 0) {
-        window.alert('[Erro] Faltam dados!')
-    }
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
+        //window.alert('[Erro] Faltam dados!')
+        
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        i = Number(inicio.value)
+        f = Number(fim.value)
+        p = Number(passo.value)
 
-    for (var i = 1; i<=fim;i++)
-    
+        if (p <= 0) {
+            window.alert('Passo invalido! Considerando passo 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for (var c = i; c <= f; c += p) {
+        
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else {
+            // Contagem regressiva
+            for (let c = i; c>= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 
     
 }
